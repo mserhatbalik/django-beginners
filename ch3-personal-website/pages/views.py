ac55d85d06ef3f5b15bb8b21360743602c388ed7
Serhat <mserhatbalik@gmail.com>
2026-05-25 15:32:35 +0300

    Created basic view and urls files.

--- ch3-personal-website/pages/views.py
+++ ch3-personal-website/pages/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# pages/view.py
+# from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home_page_view(request):
+    return HttpResponse("Homepage")
