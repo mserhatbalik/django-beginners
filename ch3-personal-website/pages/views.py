46d31742649c881ce5555e4df4d88244c4be01f8
Serhat <mserhatbalik@gmail.com>
2026-05-26 12:42:04 +0300

    Created template files and folders for about page.

--- ch3-personal-website/pages/views.py
+++ ch3-personal-website/pages/views.py
@@ -1,9 +1,13 @@
 # pages/view.py
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page_view(request):
     return HttpResponse("Homepage")
+
+
+def about_page_view(request):
+    return render(request, "pages/about.html")
