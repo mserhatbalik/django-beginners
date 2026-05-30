89c12d7478a71aa4aef4cfd83451f281d25532ec
Serhat <mserhatbalik@gmail.com>
2026-05-30 21:29:11 +0300

    Populated home template with additional functional tags

--- ch4-company/pages/views.py
+++ ch4-company/pages/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page_view(request):
-    context = {"description": "This is our glorious Company Website"}
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "Thank you for visiting!",
+    }
     return render(request, "home.html", context)
