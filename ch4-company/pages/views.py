2841b823c2f496d32574fb8ed873c233a8dd38b3
Serhat <mserhatbalik@gmail.com>
2026-05-30 20:06:21 +0300

    Added home page view and url patterns

--- ch4-company/pages/views.py
+++ ch4-company/pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page_view(request):
+    context = {"description": "This is our glorious Company Website"}
+    return render(request, "home.html", context)
