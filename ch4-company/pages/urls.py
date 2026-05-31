5bd55b228d714bb30476ed1c40aba5cc18ec0048
Serhat <mserhatbalik@gmail.com>
2026-05-31 13:45:49 +0300

    Fixed template inheritence.

--- ch4-company/pages/urls.py
+++ ch4-company/pages/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from .views import AboutPageView, home_page_view
 
-urlpatterns = [path("", home_page_view), path("about", AboutPageView.as_view())]
+urlpatterns = [
+    path("", home_page_view, name="home"),
+    path("about", AboutPageView.as_view(), name="about"),
+]
