0e4b4595d32093be9ecb254d070d9799290247e8
Serhat <mserhatbalik@gmail.com>
2026-06-19 12:37:52 +0300

    Update

--- ch6-blog/blog/admin.py
+++ ch6-blog/blog/admin.py
@@ -6,10 +6,6 @@ from .models import Post
 admin.site.register(Post)
 
 
-# class PostAdmin(admin.ModelAdmin):  # Customize admin panel post list columns
-#     list_display = ("title", "author", "body")
-
-
 class PostAdmin(admin.ModelAdmin):  # new
     list_display = (
         "title",
