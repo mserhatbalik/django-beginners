57908710a9f469172aaf471d97b5297d74f5b70a
Serhat <mserhatbalik@gmail.com>
2026-06-19 12:04:15 +0300

    Created foreign key relationship with post schema and user schema. Ask fix for gemini.

--- ch6-blog/blog/models.py
+++ ch6-blog/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     body = models.TextField
 
     def __str__(self):
