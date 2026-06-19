3ee8cf7740d4469c180ec5bbcbb88b88c9f69ff1
Serhat <mserhatbalik@gmail.com>
2026-06-19 12:37:37 +0300

    Added list display for admin post panel but not working

--- ch6-blog/blog/admin.py
+++ ch6-blog/blog/admin.py
@@ -4,3 +4,15 @@ from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+
+
+# class PostAdmin(admin.ModelAdmin):  # Customize admin panel post list columns
+#     list_display = ("title", "author", "body")
+
+
+class PostAdmin(admin.ModelAdmin):  # new
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
