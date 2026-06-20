04039d5ef37bafc1f3647febf6c4104a9afefb1e
Serhat <mserhatbalik@gmail.com>
2026-06-20 14:52:36 +0300

    Ask fix gemini

--- ch6-blog/blog/urls.py
+++ ch6-blog/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import post_list
+from .views import post_detail, post_list
 
-urlpatterns = [path("", post_list, name="home")]
+urlpatterns = [
+    path("", post_list, name="home"),
+    path("post/<int:pk>/", post_detail, name="post_detail"),
+]
