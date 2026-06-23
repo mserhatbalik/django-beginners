0b9f1fdfaad30b03e7a75818098bdbb798d063d3
Serhat <mserhatbalik@gmail.com>
2026-06-23 14:30:46 +0300

    Added post detail functionality to mainpage

--- ch6-blog-redo/blog/urls.py
+++ ch6-blog-redo/blog/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.post_list, name="home")]
+urlpatterns = [
+    path("", views.post_list, name="home"),
+    path("/post/<int:pk>", views.post_detail, name="post_detail"),
+]
--- ch6-blog-redo/blog/views.py
+++ ch6-blog-redo/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
@@ -7,3 +7,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, "post_list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_detail.html", {"post": post})
