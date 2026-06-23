78c8e2e32e641e37c1e6b7f90b78843ca24955da
Serhat <mserhatbalik@gmail.com>
2026-06-23 20:10:46 +0300

    Setup the previous blog project

---
+++ ch7-blog-forms/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
+
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, "post_list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_detail.html", {"post": post})
